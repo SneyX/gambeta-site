8bcee77936c116572f19ea2d9dcce954bb25e9eb
SneyX <tiaguitobo2@gmail.com>
2024-10-04 16:39:19 -0300

    hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,11 +8,11 @@ const Hero = () => {
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image 
-          src="/soccer-icon.png"
+          src="/camp.svg"
           alt="camp"
           width={50}
           height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[70px]"
+          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
         <h1 className="bold-52 lg:bold-88">Gambeta</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
@@ -34,19 +34,19 @@ const Hero = () => {
 
           <p className="bold-16 lg:bold-20 text-blue-70">
             198k
-            <span className="regular-16 lg:regular-20 ml-1">Reseñas Excelentes</span>
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
           </p>
         </div>
 
         <div className="flex flex-col w-full gap-3 sm:flex-row">
           <Button 
             type="button" 
-            title="Descargar App" 
+            title="Download App" 
             variant="btn_green" 
           />
           <Button 
             type="button" 
-            title="¿Cómo Funciona?" 
+            title="How we work?" 
             icon="/play.svg"
             variant="btn_white_text" 
           />
@@ -58,7 +58,7 @@ const Hero = () => {
 
            <div className="flex flex-col">
             <div className="flexBetween">
-              <p className="regular-16 text-gray-20">Ubicación</p>
+              <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
             <p className="bold-20 text-white">Aguas Calientes</p>
@@ -66,11 +66,11 @@ const Hero = () => {
 
           <div className="flexBetween">
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Distancia</p>
+              <p className="regular-16 block text-gray-20">Distance</p>
               <p className="bold-20 text-white">173.28 mi</p>
             </div>
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">Elevación</p>
+              <p className="regular-16 block text-gray-20">Elevation</p>
               <p className="bold-20 text-white">2.040 km</p>
             </div>
           </div>
