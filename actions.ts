987a5f490872ac2cc763dca5c58426b54271463d
SneyX <tiaguitobo2@gmail.com>
2024-10-28 16:39:35 -0300

    server actions

--- actions.ts
+++ actions.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string, {
