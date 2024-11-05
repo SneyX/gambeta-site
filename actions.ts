ce75e3351bd803ab041fb047b3838e53b11de8c2
SneyX <tiaguitobo2@gmail.com>
2024-11-05 20:20:09 -0300

    keys

--- actions.ts
+++ actions.ts
@@ -3,7 +3,7 @@
 import { createClient } from "@supabase/supabase-js";
 import * as crypto from 'crypto';
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY as string, {
+const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_SERVICE_ROLE_KEY as string, {
     auth: {
       autoRefreshToken: false,
       persistSession: false
